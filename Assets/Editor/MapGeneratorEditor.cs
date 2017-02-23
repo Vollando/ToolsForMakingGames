5be3cb1e26f0663cf0e1982db2d96d5472ffd101
Vollando <cvb.charles@hotmail.com>
2017-02-23 15:39:24 +0000

    noise map generation

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapScript))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapScript mapGen = (MapScript)target;
+
+        if (DrawDefaultInspector())
+        {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/MapScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour {
+
+	public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = NoiseScript.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+
+        MapDisplayScript display = FindObjectOfType<MapDisplayScript>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+}
