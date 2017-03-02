123b72987eef13d8443eaea4b36df6324c0bdf44
Vollando <cvb.charles@hotmail.com>
2017-03-02 12:50:57 +0000

    added Octaves, Persistance and Seed arguments

--- Assets/Scripts/MapScript.cs
+++ Assets/Scripts/MapScript.cs
@@ -8,15 +8,43 @@ public class MapScript : MonoBehaviour {
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = NoiseScript.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = NoiseScript.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
 
         MapDisplayScript display = FindObjectOfType<MapDisplayScript>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 
 }
