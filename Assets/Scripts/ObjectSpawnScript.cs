66516a9bbe3d3792a847d421d1142f4e6465faae
Vollando <cvb.charles@hotmail.com>
2017-04-21 14:29:45 +0100

    starting object spawning script

---
+++ Assets/Scripts/ObjectSpawnScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnScript : MonoBehaviour {
+
+    public bool spawnEnabled = false;
+
+    public TerrainType[] regions;
+
+    public void ObjectSpawn()
+    {
+        for (int i = 0; i < regions.Length; i++)
+        {
+            if (currentHeight <= regions[i].height)
+            {
+
+        }	
+}
