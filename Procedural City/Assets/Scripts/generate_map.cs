3eb13df1c25029b7e4fed64331b8504286654cff
Vollando <cvb.charles@hotmail.com>
2017-05-03 03:40:55 +0100

    Region detection, new models

--- Procedural City/Assets/Scripts/generate_map.cs
+++ Procedural City/Assets/Scripts/generate_map.cs
@@ -7,6 +7,8 @@ public class generate_map : MonoBehaviour {
     
     public GameObject[] buildings;
     public GameObject[] trees;
+    public bool placeModels;
+    public bool showBorder;
     public enum DrawMode { NoiseMap, ColorMap, Mesh};
     public DrawMode draw_mode;
     
@@ -69,16 +71,16 @@ public class generate_map : MonoBehaviour {
                 {
                     allSatisfied = true;
                     int[] coords = new int[4] { i, j - S[i, j] + 1, i - S[i, j] + 1, j };
-                    Debug.Log("w started at " + i.ToString() + " and ended at " + (i - S[i, j]).ToString());
-                    Debug.Log("h started at " + (j - S[i, j] + 1).ToString() + " and ended at " + (j - 1).ToString());
+                    //Debug.Log("w started at " + i.ToString() + " and ended at " + (i - S[i, j]).ToString());
+                    //Debug.Log("h started at " + (j - S[i, j] + 1).ToString() + " and ended at " + (j - 1).ToString());
                     for (int w = i - S[i, j] + 1; w < i && allSatisfied; ++w)
                     {
                         for (int h = j - S[i, j] + 1; h < j && allSatisfied; ++h)
                         {
-                            Debug.Log("got into loop");
+                           // Debug.Log("got into loop");
                             if (occupied[w, h] == 1)
                             {
-                                Debug.Log("Set to False");
+                               // Debug.Log("Set to False");
                                 allSatisfied = false;
                             }
                             else
@@ -98,36 +100,77 @@ public class generate_map : MonoBehaviour {
     {
         // have to force it to low value
         mapChunkSize = (fill ? 101 : 241);
+        float heightCheck = regions[0].height;
+        List<int> treePositions = new List<int>();
 
-        float[,] map = perlinMap.generateMap(mapChunkSize, mapChunkSize, seed, scale, octaves, persistence, 
+        float[,] map = perlinMap.generateMap(mapChunkSize, mapChunkSize, seed, scale, octaves, persistence,
             lacunarity, offset);
-        
+
         // store whether that area is livable
-        int [,] livable = new int [mapChunkSize, mapChunkSize];
+        int[,] livable = new int[mapChunkSize, mapChunkSize];
 
         Color[] colormap = new Color[mapChunkSize * mapChunkSize];
-        for(int h = 0; h < mapChunkSize; ++h )
+        for (int h = 0; h < mapChunkSize; ++h)
         {
-            for(int w = 0; w < mapChunkSize; ++w)
+            for (int w = 0; w < mapChunkSize; ++w)
             {
                 // begin searching for livable areas
                 if (map[w, h] <= cityHigh && map[w, h] >= cityLow)
                     livable[w, h] = 1;
+                    
                 else
-                    livable[w, h] = 0;         
+                    livable[w, h] = 0;
 
                 float currentHeight = map[w, h];
-                for(int i = 0; i < regions.Length; ++i)
+
+                for (int i = 0; i < regions.Length; ++i)
                 {
-                    if(currentHeight <= regions[i].height)
+                    if (currentHeight <= regions[i].height)
                     {
-                        colormap[h * mapChunkSize + w] = regions[i].color;
+                        Color colour = regions[i].color;
+
+                        if (i == 1 && showBorder)
+                        {
+                            // check left
+                            if (w > 0 && map[w - 1, h] <= heightCheck)
+                            {
+                                //colormap[h * mapChunkSize + w - 1] = Color.black;
+                                treePositions.Add(h * mapChunkSize + (w - 1));
+                            }
+                            // check top
+                            if (h > 0 && map[w, h - 1] <= heightCheck)
+                            {
+                                //colormap[(h - 1) * mapChunkSize + w] = Color.black;
+                                treePositions.Add((h-1) * mapChunkSize + w);
+                            }
+                        }
+                        else if (i == 0 && showBorder)
+                        {
+                            // check left
+                            if (w > 0 && map[w - 1, h] > heightCheck)
+                            {
+                                treePositions.Add(h * mapChunkSize + w);
+                                //colour = Color.black;
+                            }
+                            // check top
+                            if (h > 0 && map[w, h - 1] > heightCheck)
+                            {
+                                treePositions.Add(h * mapChunkSize + w);
+                                //colour = Color.black;
+                            }
+                        }
+                        //else if (i == 3 && placeModels)
+                        //{
+                        //    treePositions.Add(h * mapChunkSize + w);
+                        //}
+
+                        colormap[h * mapChunkSize + w] = colour;
                         break;
                     }
                 }
             }
         }
-                
+
         display_map display = FindObjectOfType<display_map>();
 
         if (draw_mode == DrawMode.NoiseMap)
@@ -135,90 +178,101 @@ public class generate_map : MonoBehaviour {
         else if (draw_mode == DrawMode.ColorMap)
             display.DrawTexture(TextureGenerator.TextureFromColorMap(colormap, mapChunkSize, mapChunkSize));
         else if (draw_mode == DrawMode.Mesh)
-        {            
-         
-            display.DrawMesh(MeshGenerator.generate_terrain_mesh(map, meshHeightMultiplier,
-                levelOfDetails, meshHeightCurve),
-                TextureGenerator.TextureFromColorMap(colormap, mapChunkSize, mapChunkSize));
+        {
+            MeshData md = MeshGenerator.generate_terrain_mesh(map, meshHeightMultiplier, levelOfDetails, meshHeightCurve);
 
-            if (fill)
+            display.DrawMesh(md, TextureGenerator.TextureFromColorMap(colormap, mapChunkSize, mapChunkSize));
+
+            for (int i = 0; i < treePositions.Count; ++i)
             {
-                // plant some vegetations!
-                // find the max value in map
-                int max_w = -1, max_h = -1;
-                float max = -1f;
+                Vector3 pos = md.vertices[treePositions[i]];
+                //pos.x *= 10;
+                //pos.z *= 10;
 
-                for (int w = 0; w < mapChunkSize; ++w)
+
+                GameObject tree = Instantiate(trees[0], pos, Quaternion.identity);
+            }
+
+               // if (fill)
                 {
-                    for (int h = 0; h < mapChunkSize; ++h)
-                    {
-                        if (map[w, h] > max)
-                        {
-                            max = map[w, h];
-                            max_w = w;
-                            max_h = h;
-                        }
-                        // generate some stuff on the ground
-                        if (map[w, h] >= 0.3f && map[w, h] <= 0.5f)
-                        {
-                            float dice = Random.Range(0.0f, 1.0f);
+                    // plant some vegetations!
+                    // find the max value in map
+                    int max_w = -1, max_h = -1;
+                    float max = -1f;
 
-                            if (dice < 0.08f)
+                    for (int w = 0; w < mapChunkSize; ++w)
+                    {
+                        for (int h = 0; h < mapChunkSize; ++h)
+                        {
+                            if (map[w, h] > max)
                             {
-                                Vector3 treePos = new Vector3(w,
-                                    meshHeightCurve.Evaluate(map[w, h]) * meshHeightMultiplier,
-                                    mapChunkSize - h);
-                                Instantiate(trees[0], treePos, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                                max = map[w, h];
+                                max_w = w;
+                                max_h = h;
                             }
-            
-                        }
-                    }
-                }
-                // procedurally generate city
-                List<int[]> coords = squares(livable, villageSizeCutoff);
-
-                foreach (int[] i in coords)
-                {
-                    int cityWidth = Mathf.Abs(i[3] - i[1]);
-                    int cityHeight = Mathf.Abs(i[0] - i[2]);
-                    int buildingFootprint = 2;
-
-                    int[,] citygrid = new int[cityWidth, cityHeight];
-                    for (int h = 0; h < cityHeight; ++h)
-                    {
-                        for (int w = 0; w < cityWidth; ++w)
-                        {
-                            citygrid[w, h] = (int)(Mathf.PerlinNoise(w / 10.0f, h / 10.0f) * 10.0f);
-                        }
-                    }
-
-                    // build city
-
-                    float mapY = meshHeightCurve.Evaluate(map[i[0], i[1]]) * meshHeightMultiplier;
-                    for (int h = 0; h < cityHeight; h++)
-                    {
-                        for (int w = 0; w < cityWidth; w++)
-                        {
-
-                            int result = citygrid[w, h];
-                            Debug.Log(result);
-                            Vector3 position = new Vector3(w * buildingFootprint + i[0], mapY, mapChunkSize - (h * buildingFootprint + i[1]));
-                            if (w * buildingFootprint < cityWidth && h * buildingFootprint < cityHeight)
+                            // generate some stuff on the ground
+                            if (map[w, h] >= 0.3f && map[w, h] <= 0.5f)
                             {
-                                if (result < 2)
-                                    Instantiate(buildings[0], position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-                                else if (result < 4)
-                                    Instantiate(buildings[1], position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-                                else if (result < 6)
-                                    Instantiate(buildings[2], position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-                                else if (result < 8)
-                                    Instantiate(buildings[3], position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
-                                else if (result < 10)
-                                    Instantiate(buildings[4], position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+                                float dice = Random.Range(0.0f, 1.0f);
+
+                                if (dice < 0.08f)
+                                {
+                                    Vector3 treePos = new Vector3(w - 50.0f,
+                                        meshHeightCurve.Evaluate(map[w, h]) * meshHeightMultiplier,
+                                        (mapChunkSize - h) - 50.0f);
+                                    Instantiate(trees[0], treePos, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                                }
+                            }
+                            else if (map[w, h] >= 0.83f && map[w, h] <= 0.87f)
+                            {
+                                    Vector3 treePos = new Vector3(w - 50.0f,
+                                        meshHeightCurve.Evaluate(map[w, h]) * meshHeightMultiplier,
+                                        (mapChunkSize - h) - 50.0f);
+                                    Instantiate(trees[0], treePos, Quaternion.identity);
+                            }
+                    }
+                    }
+                    // procedurally generate city
+                    List<int[]> coords = squares(livable, villageSizeCutoff);
+
+                    foreach (int[] j in coords)
+                    {
+                        int cityWidth = Mathf.Abs(j[3] - j[1]);
+                        int cityHeight = Mathf.Abs(j[0] - j[2]);
+                        int buildingFootprint = 2;
+
+                        float[,] citygrid = new float[cityWidth, cityHeight];
+                        for (int h = 0; h < cityHeight; ++h)
+                        {
+                            for (int w = 0; w < cityWidth; ++w)
+                            {
+                            //citygrid[w, h] = (int)(Mathf.PerlinNoise(w / 10.0f, h / 10.0f) * 10.0f);
+                            citygrid[w, h] = Random.Range(0.0f, 1.0f);
                             }
                         }
+
+                        // build city
+
+                        float mapY = meshHeightCurve.Evaluate(map[j[0], j[1]]) * meshHeightMultiplier;
+                        for (int h = 0; h < cityHeight; h++)
+                        {
+                            for (int w = 0; w < cityWidth; w++)
+                            {
+                                float result = citygrid[w, h];
+                                Debug.Log(result);
+                                Vector3 position = new Vector3((w * buildingFootprint + j[0]) - 50.0f, mapY, (mapChunkSize - (h * buildingFootprint + j[1])) - 50.0f);
+                                if (w * buildingFootprint < cityWidth && h * buildingFootprint < cityHeight)
+                                {
+                                if (result < 0.3f)
+                                    Instantiate(buildings[0], position, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                                else if (result > 0.3f && result < 0.7f)
+                                        Instantiate(buildings[1], position, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                                    else if (result > 0.7f)
+                                        Instantiate(buildings[2], position, Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
+                            }
+                            }
+                        }
                     }
-                }
             }
         }
     }
